8e8d07ef8de5235b66bdfbc22baf43e76899e402
Dengus1 <130084870+Dengus1@users.noreply.github.com>
2024-05-26 11:52:13 +0200

    0.0.5

--- js/main.js
+++ js/main.js
@@ -5,3 +5,53 @@ burgerIcon.addEventListener('click', function(){
     mobileNav.classList.toggle('active');
     burgerIcon.classList.toggle('active');
 })
+
+
+// disable rightclick
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+document.onkeydown = function (e) {
+
+// disable F12 key
+if(e.keyCode == 123) {
+alert("Halo f12!?");
+    return false;
+}
+
+
+// disable c key
+if(e.ctrlKey && e.keyCode == 67){
+    alert("Rabuś nie kradnij!!");
+    return false;
+
+}
+
+    // disable c key
+if(e.ctrlKey && e.keyCode == 67){
+    alert("Rabuś nie kradnij!!");
+    return false;
+
+}
+
+
+// disable i key
+if(e.ctrlKey && e.keyCode == 73) {
+alert("ni ma");
+    return false;
+} 
+
+// disable j key
+if(e.ctrlKey && e.keyCode == 74) {
+alert("Nie tak prędko");
+    return false;
+}
+
+    // disable p key
+if(e.ctrlKey && e.keyCode == 80){
+    alert("ty chcesz to drukowac? xdd");
+    return true;
+    alert("Trzymaj i powieś na ścianie");
+
+}
+
+}
